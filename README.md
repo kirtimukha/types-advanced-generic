cf70cc2c3b302b9455888e78aa14b377d3c1f529
stellamax <kirtimukha@nate.com>
2023-06-29 19:24:09 +0900

    init

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Advanced TypeScript: Generic Search, Sorting and Filtering
 
+설치: 14.13.1 을 다운로드 할 수 없고, 14의 최신 버전도 패키지 설치가 안됨
+19.18.1 노드 상태에서 타입스크립트 템플릿 설치후. node-sass, bootstrap 추가 설치하여 진행함
+
 This is the code repository for my course "Advanced TypeScript: Generic Search, Sorting and Filtering", available on [Skillshare](https://skl.sh/3oGQMbr) and [Udemy](https://www.udemy.com/course/advanced-typescript-generic-search-sorting-and-filtering/?referralCode=22441D8B6B06045473D2).
 
 A preview of the course (6 of the 19 lessons) is also available on [YouTube](https://www.youtube.com/playlist?list=PLOX_mQri1cCleaUEI_k2qDaWFpnA7wW0z).
---
+++ src/generic/generics.ts
@@ -0,0 +1,94 @@
+export default interface IFooBar {
+  bar: string;
+  foo: string;
+}
+
+const fooBars: Array<IFooBar> = [
+  {
+    foo: "foo 1",
+    bar: "bar 1",
+  },
+  {
+    foo: "i am foo two",
+    bar: "i am bar tow",
+  },
+  {
+    foo: "foo two",
+    bar: "bar two",
+  },
+];
+function sortByFoo(fooBars: Array<IFooBar>) {
+  fooBars.sort((a, b) => {
+    if (a.foo > b.foo) {
+      return 1;
+    }
+    if (a.foo < b.foo) {
+      return -1;
+    }
+
+    return 0;
+  });
+}
+
+function sortByBar(fooBars: Array<IFooBar>) {
+  fooBars.sort((a, b) => {
+    if (a.bar > b.bar) {
+      return 1;
+    }
+    if (a.bar < b.bar) {
+      return -1;
+    }
+    return 0;
+  });
+}
+console.log(sortByFoo(fooBars)); // always get undefined
+console.log(sortByBar(fooBars)); // always get undefined
+
+function sortByKey<T>(data: Array<T>, key: keyof T) {
+  data.sort((a, b) => {
+    if (a[key] > b[key]) {
+      return 1;
+    }
+    if (a[key] < b[key]) {
+      return -1;
+    }
+    return 0;
+  });
+}
+sortByKey<IFooBar>(fooBars, "foo");
+console.log(sortByKey<IFooBar>(fooBars, "foo"));
+
+class Animal {
+  public legCount: number;
+  constructor(legCount: number) {
+    this.legCount = legCount;
+  }
+}
+
+class Cat extends Animal {
+  constructor() {
+    super(4);
+  }
+}
+class Chiken extends Animal {
+  constructor() {
+    super(2);
+  }
+}
+class Bacteria extends Animal {
+  constructor() {
+    super(0);
+  }
+}
+function printLegCount<T extends Animal>(animal: T): number {
+  console.log(`My leg count is: ${animal.legCount}`);
+  return animal.legCount;
+}
+const myCat = new Cat();
+printLegCount(myCat);
+
+const myChiken = new Chiken();
+printLegCount(myChiken);
+
+const myBacteria = new Bacteria();
+printLegCount(myBacteria);
